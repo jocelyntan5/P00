8427462708a39c96974d494954cffd349ba279ec
jocelyntan5 <22043453@myrp.edu.sg>
2023-05-26 14:40:08 +0800

    first push to P00 repository in GitHub

---
+++ P00/src/Calculator.java
@@ -0,0 +1,19 @@
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int subract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+}
